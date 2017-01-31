388cee31b0f0be323135b7cccc24beab50db263a
laksmitawidya <mithawid@gmail.com>
2017-01-31 12:39:59 +0700

    automate login in stageops

---
+++ src/com/selenium/tests/HotelPositivePromo.java
@@ -0,0 +1,11 @@
+package com.selenium.tests;
+
+import org.openqa.selenium.WebDriver;
+
+public class HotelPositivePromo {
+	public static void main(String[] args) {
+		WebDriver driver;
+		System.setProperty("webdriver.gecko.driver", "E:/SoftwareQA/geckodriver/geckodriver.exe");
+		
+	}
+}
